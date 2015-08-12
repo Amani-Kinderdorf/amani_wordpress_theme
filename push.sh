9bebc40ddb6a50bd596baad4c9b20638c2cf0ef9
CookingRobot <friedrich@cookingrobot.de>
2015-08-12 20:46:25 +0200

    modify push script

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo 'Publishing'
+echo 'Publishing...'
 mysqldump --user root --password=123 amani_wordpress > amani_wordpress.sql
 git add amani_wordpress.sql
 git commit -m "auto: add database_dump"
 git push
+echo '✅ Done'
