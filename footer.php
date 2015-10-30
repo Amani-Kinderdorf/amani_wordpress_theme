f4dd2ac62e26fbe438e6906109f0b92a8df61307
CookingRobot <friedrich@cookingrobot.de>
2015-10-30 16:13:15 +0100

    only load custom jquery on front page; reduces page load time und fixes #15

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 ©  2015  Amani Kinderdorf e.V. | <a href="<?php echo get_page_link(85); ?>">Impressum</a>
 </footer>
 <?php endif; ?>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.2.min.js"></script>
+
 <script type="text/javascript">
     function searchButtonClicked () {
         if(document.getElementById('searchField').value.length>0) document.getElementById('searchForm').submit();
@@ -16,6 +16,7 @@
     }
 </script>
  <?php if(is_front_page()||is_home()): ?>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_url')?>/js/slick.min.js"></script>
 	<script type="text/javascript">	
 	    $(document).ready(function(){
