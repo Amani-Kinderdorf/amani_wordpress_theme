6416782cfd50a506cef29edc4206c3cefe9d0dd8
CookingRobot <friedrich@cookingrobot.de>
2015-10-30 15:43:10 +0100

    hide post title on mobile devices

--- index.php
+++ index.php
@@ -8,17 +8,7 @@
         <?php get_sidebar(); ?>
         
         <article class="pageContentViewItem pageStyle">
-
-        	<script>
-        	/* lets the heading of the pages 'Über uns', 'Kinderdörfer' und 'Unterstützen' disappear for responsive Design  */
-        	var maxScreenWidth = 768;
-        	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-	    		if (width > maxScreenWidth || document.title != 'Über uns' || document.title != 'Kinderdörfer' || document.title != 'Unterstützen') {
-	    			document.write("<h1><?php wp_reset_query(); the_title(); ?></h1>");
-	    		}
-	    	</script>
-
+            <h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
             <?php the_content(); ?>
          </article>
      </div>
