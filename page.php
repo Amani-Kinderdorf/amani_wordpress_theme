a13fedd975426035266e70f0d4cb1f9e7db0fdc0
CookingRobot <friedrich@cookingrobot.de>
2015-11-02 20:47:35 +0100

    first fragible mailman form implmentation

--- page.php
+++ page.php
@@ -10,6 +10,7 @@
             <h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
 
             <?php the_content(); ?>
+           	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : endif; ?>
          </article>
      </div>
       
