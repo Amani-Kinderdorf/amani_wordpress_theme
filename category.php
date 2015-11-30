e90ce08361ca90a73a0ed63fdd5642e0251b07e8
CookingRobot <hi@weise.io>
2015-11-30 17:43:53 +0100

    change pagination text to germn (#22)

--- category.php
+++ category.php
@@ -39,8 +39,8 @@
 
 				<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
 				                <div id="nav-below" class="navigation">
-				                    <div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'hbd-theme' )) ?></div>
-				                    <div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'hbd-theme' )) ?></div>
+				                    <div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Ältere Artikel', 'hbd-theme' )) ?></div>
+				                    <div class="nav-next"><?php previous_posts_link(__( 'Neuere Artikel <span class="meta-nav">&raquo;</span>', 'hbd-theme' )) ?></div>
 				                </div><!-- #nav-below -->
 				<?php } ?>                 
  
