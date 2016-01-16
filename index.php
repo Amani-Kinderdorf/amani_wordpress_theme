0989ab6174d14e23139e033ff73240544a51c3ea
CookingRobot <hi@weise.io>
2016-01-16 12:53:35 +0100

    same layout for posts

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
-
 <?php get_header(); ?>
-
 
 <div class="contentWrapper mainWrapper">
      <div class="xColumnView">
-
         <?php get_sidebar(); ?>
-        
         <article class="pageContentViewItem pageStyle">
-            <h1><?php wp_reset_query(); the_title(); ?></h1>
+            <div class="postHeader">
+            	<h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
+            	<p class="postMeta"><?php the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
             <?php the_content(); ?>
          </article>
      </div>
  </div>
-
-
-
-
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -6,7 +6,7 @@
         <article class="pageContentViewItem pageStyle">
             <div class="postHeader">
             	<h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
-            	<p class="postMeta"><?php the_date('d. F Y'); ?> - <?php echo get_categorie_simple(get_the_ID()); ?></p>
+            	<p class="postMeta"><?php the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
             <?php the_content(); ?>
          </article>
      </div>
