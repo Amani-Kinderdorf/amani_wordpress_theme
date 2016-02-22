a3eb27bc695071ea82c9743e2d976bb9c8abff9a
CookingRobot <hi@weise.io>
2016-02-22 16:23:08 +0100

    fix redirect

--- template_redirect.php
+++ template_redirect.php
@@ -4,6 +4,7 @@ Template Name: Redirect
 */?>
 
 <?php 
-$url = get_term_link(get_field('url'),'category');
-wp_redirect($url); 
-exit; ?>
+	$url = get_term_link(get_field('url')[0],'category');
+	wp_redirect($url); 
+	exit; 
+?>
