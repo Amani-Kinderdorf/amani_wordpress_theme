1127de2b76a23c4963b4959a05d5762899fcb0ba
CookingRobot <hi@weise.io>
2016-04-10 12:06:49 +0300

    nicer code

--- landing_page.php
+++ landing_page.php
@@ -23,7 +23,8 @@ Template Name: Startseite
  				while( $loop->have_posts() ) : 
 				$loop->the_post(); ?>
 				<div class="newsPreview">
-					<p class="title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></p><p class="date"><?php echo get_the_date('d. F');?></p>
+					<p class="title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></p>
+					<p class="date"><?php echo get_the_date('d. F');?></p>
 				</div><?php	
    				endwhile;
    				wp_reset_query();
