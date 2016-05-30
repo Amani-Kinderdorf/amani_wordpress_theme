6b0e652bf89baa4fda6760c23a695d7e017a9d88
CookingRobot <hi@weise.io>
2016-05-30 14:32:18 +0200

    change slideshow speed

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@ Amani Kinderdorf e.V.<?php wp_nav_menu( array('menu' => 'footerMenu', 'container
 	    jQuery(document).ready(function(){
 	       jQuery('#slideShowContent').slick({
 	        autoplay: true,
-	        autoplaySpeed: 3000,  
+	        autoplaySpeed: 4000,  
 	        infinite: true,
 	        speed: 400,
 	        fade: true,
