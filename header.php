2a103c3c0179c26b1aa8fbea1660a8fc7f37438b
CookingRobot <hi@weise.io>
2016-07-27 21:39:05 +0200

    update and clean up favicons

--- header.php
+++ header.php
@@ -30,20 +30,14 @@
     <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'hbd-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url')?>/img/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('template_url')?>/img/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url')?>/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_url')?>/img/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url')?>/img/favicons/favicon-16x16.png">
-
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url')?>/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_url')?>/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_url')?>/img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php bloginfo('template_url')?>/img/favicons/manifest.json">
+    <link rel="mask-icon" href="<?php bloginfo('template_url')?>/img/favicons/safari-pinned-tab.svg" color="#a80319">
+    <link rel="shortcut icon" href="<?php bloginfo('template_url')?>/img/favicons/favicon.ico">
+    <meta name="msapplication-config" content="<?php bloginfo('template_url')?>/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
     <?php wp_head(); ?>
 </head>
 <body>
