4e38f5b6d87764d48314d3d4e4fdcb3b101f372c
CookingRobot <hi@weise.io>
2016-07-28 09:46:47 +0200

    redesign sidebar part I, introduce default padding variable

--- page.php
+++ page.php
@@ -6,8 +6,8 @@
         
         <?php get_sidebar(); ?>
 
-         <article class="pageContentViewItem <?php if(count($pages)==0) echo " oneColumnViewItem "; ?>pageStyle">
-            <h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
+        <article class="pageContentViewItem <?php if(count($pages)==0) echo " oneColumnViewItem "; ?>pageStyle">
+            <h1 class="postHeading postHeadingPage"><?php wp_reset_query(); the_title(); ?></h1>
 
             <?php the_content(); ?>
            	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : endif; ?>
