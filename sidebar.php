a587f85264d92c15e24a6407bc7aa4f4dc1e0739
CookingRobot <hi@weise.io>
2016-08-04 00:05:42 +0200

    layout improvements; new footer design

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 //sidebar for posts
 if( is_archive() || is_category() ||  is_single() || is_home() || is_page_template('archive_template.php')): ?>
             <aside class="sideBarViewItem sideBarPageTree">
-                 <li id="aktuelle-berichte" class="page_item page_item_has_children">
+                <li id="aktuelle-berichte" class="page_item page_item_has_children">
                     <a class="sideBarPageTreeHeading" 
                         onclick="
                         this.nextElementSibling.classList.toggle('sideBarPageTreeItems--visible');
@@ -12,31 +12,31 @@ if( is_archive() || is_category() ||  is_single() || is_home() || is_page_templa
                         Aktuelle Berichte
                         <span class="showMoreButton"></span>
                     </a>
-                        <ul class="children sideBarPageTreeItems">
-                            <?php 
-                            //custom loop for news
-                            $current = get_the_title();
-                            $args = array( 'posts_per_page' => get_option('posts_per_page'), 'orderby' => 'date' , 'order'   => 'DESC');
-                            $loop = new WP_Query($args);
-                                while( $loop->have_posts() ) : 
-                                    $loop->the_post(); 
-                                    if(strcmp($current,get_the_title())==0 && is_category()==false) $current_item = "current_page_item";
-                                    else $current_item = "";?>
-                            <li class="page_item <?php echo $current_item; ?>">
-                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-                            </li><?php  
-                                endwhile;
-                                wp_reset_query();
-                            ?>
-                        </ul>
-                    </li>
-                    <li class="page_item page_item_has_children <?php if(is_page_template('archive_template.php')) echo 'current_page_item'; ?> sideBarPageTreeItems">
-                        <a href="<?php echo getArchiveLink() ?>">Archiv</a>
-                        <ul class="children">
-                            <li class="cat-item"><a href="<?php echo getArchiveLink() ?>">alle Kategorien</a></li>
-                            <?php wp_list_categories('title_li='); ?>
-                        </ul>
-                    </li>
+                    <ul class="children sideBarPageTreeItems">
+                        <?php 
+                        //custom loop for news
+                        $current = get_the_title();
+                        $args = array( 'posts_per_page' => get_option('posts_per_page'), 'orderby' => 'date' , 'order'   => 'DESC');
+                        $loop = new WP_Query($args);
+                            while( $loop->have_posts() ) : 
+                                $loop->the_post(); 
+                                if(strcmp($current,get_the_title())==0 && is_category()==false) $current_item = "current_page_item";
+                                else $current_item = "";?>
+                        <li class="page_item <?php echo $current_item; ?>">
+                            <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                        </li><?php  
+                            endwhile;
+                            wp_reset_query();
+                        ?>
+                    </ul>
+                </li>
+                <li class="page_item page_item_has_children <?php if(is_page_template('archive_template.php')) echo 'current_page_item'; ?> sideBarPageTreeItems">
+                    <a href="<?php echo getArchiveLink() ?>">Archiv</a>
+                    <ul class="children">
+                        <li class="cat-item"><a href="<?php echo getArchiveLink() ?>">alle Kategorien</a></li>
+                        <?php wp_list_categories('title_li='); ?>
+                    </ul>
+                </li>
         </aside>
 <?php
 
