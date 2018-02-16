28b3a8de77ecc7968ce4f5d5dcf701c31bfc3447
friedrichweise <account@weise.io>
2018-02-16 15:13:58 +0100

    remove unused hreflang tag

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
     <meta name="author" content="Amani Kinderdorf e.V." />
     <meta name="description" content="Der Verein Amani Kinderdorf e.V. baut und betreibt zwei Dörfer für bedürftige Kinder in der Region Iringa im Süden Tansanias und fördert die schulische und berufliche Bildung dieser Kinder." />
     <meta name="google-site-verification" content="AIq1kRfS_3ruMWJtQIDk2Ts8YGAJx4ib1ZZJRN_I4Ds" />
-    <link rel="alternate" href="https://www.amani-kinderdorf.de" hreflang="de" />
     <meta name="keywords" content="tanzania, tansania, kilolo, kitwiru, amani, kinderdorf, freiwilligendienst, Patenschaft, weltwärts, Amani Kinderdorf, Amani-Kinderdorf, Amani Kinderdorf e.V., Amani Kinderdorf, Iringa, RUCU, Computer, Bildung, Schule, Schreiner, Schreinerei, Tischler. Tischlerei, Geldern" />
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/style.css" />    
     <?php if(is_front_page()||is_home()): ?>
