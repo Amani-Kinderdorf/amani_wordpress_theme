018f9971db481bd6c56185da7e75277bdcdfcd2b
friedrichweise <account@weise.io>
2018-06-12 10:16:53 +0200

    fix regex syntax and use global keyword to access validations

--- helpers/form_validations.php
+++ helpers/form_validations.php
@@ -6,11 +6,11 @@ add_filter( 'wpcf7_validate_text', 'custom_text_filter', 20, 2 );
 // source: https://stackoverflow.com/a/15920158
 $validations = [
 	'betrag' => [
-		'reg' => '^(\d+((\.|,)\d{2})?)$', 
+		'reg' => '/^(\d+((\.|,)\d{2})?)$/', 
 		'message' => 'Bei der Betragseingabe nur Zahlen eingeben, oder ein Komma zur Abtrennung der Cents (z.B. 12 oder 12,00).'
 	],
 	'iban' => [
-		'reg' => '^DE\d{2}\s?([0-9a-zA-Z]{4}\s?){4}[0-9a-zA-Z]{2}$',
+		'reg' => '/^DE\d{2}\s?([0-9a-zA-Z]{4}\s?){4}[0-9a-zA-Z]{2}$/',
 		'message' => 'Die eingegebene IBAN enthält einen Fehler.'
 	],
 	'bic' => [
@@ -20,6 +20,7 @@ $validations = [
 ];
 
 function custom_text_filter( $result, $tag ) {
+	global $validations;
 	foreach ($validations as $key => $validation) {
 		//key exists
 		if($key == $tag->name) {
