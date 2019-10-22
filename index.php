a9589eff550d32e88fde9474da7ebfdfd0d98dcf
friedrichweise <account@weise.io>
2019-10-22 18:20:56 +0200

    use more robust get_the_date function

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 					</div>
 				<?php endif ?>
 				<div class="articlePreview__text">
-            		<p class="postMeta"><?php the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
+            		<p class="postMeta"><?php echo get_the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
 					<h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
 					<p><?php the_excerpt(); ?></p>
 				</div>
--- single.php
+++ single.php
@@ -5,7 +5,7 @@
         <?php get_sidebar(); ?>
         <article class="pageContentViewItem pageStyle">
             <div class="postHeader">
-            	<p class="postMeta"><?php the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
+            	<p class="postMeta"><?php echo get_the_date('d. F Y'); ?> | <?php echo get_categorie_simple(get_the_ID()); ?></p>
             	<h1 class="postHeading"><?php wp_reset_query(); the_title(); ?></h1>
             <?php the_content(); ?>
          </article>
