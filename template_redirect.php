9782fb111f60990d3f118eed12d1e2a81ae5487a
friedrichweise <account@weise.io>
2020-09-04 16:49:21 +0200

    fix redirect template

--- template_redirect.php
+++ template_redirect.php
@@ -1,9 +1,8 @@
 <?php
 /*
 Template Name: Redirect
-*/?>
+*/
 
-<?php 
 	$url = get_term_link(get_field('url')[0],'category');
 	if(get_field('url_link')) $url = get_field('url_link');
 	wp_redirect($url); 
