98d63eacbfd7414de79090dff09203a99d099262
friedrich <friedrichweise@users.noreply.github.com>
2021-03-08 17:45:41 +0100

    increase max upload file size

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@
 require_once('helpers/form_validations.php');
 require_once('helpers/pagination.php');
 
+//increase max file size
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 //disable emoji script
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
